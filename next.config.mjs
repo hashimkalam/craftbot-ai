6d909ca870eeac9cc533f61c7fe89b2a7eae3f58
hashimkalam <hashim.20211291@iit.ac.lk>
2024-10-22 21:45:20 +0530

    adjusted next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/graphql",
+        destination:
+          "https://assistly-ai-8sw6-pnnx8w3v0-hashim-kalams-projects.vercel.app/api/graphql",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
