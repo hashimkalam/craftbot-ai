de1a175b6592b992bc7714ce55a820c64850625d
hashimkalam <hashim.20211291@iit.ac.lk>
2024-10-22 21:27:25 +0530

    cors issue update

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -3,17 +3,19 @@ import { gql, ApolloError } from "@apollo/client";
 import { NextRequest, NextResponse } from "next/server";
 
 const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": "https://craftbot-ai.vercel.app",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
+// Handle preflight requests
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
 export async function POST(request: NextRequest) {
   // Extract query and variables from JSON body
   const { query, variables } = await request.json();
-
-  // console.log("Received query: ", query);
-  // console.log("Received variables: ", variables);
 
   try {
     let result;
@@ -22,30 +24,35 @@ export async function POST(request: NextRequest) {
       throw new Error("Invalid query format.");
     }
 
-    // Validate and sanitize query (optional step)
-
+    // Execute the GraphQL query or mutation
     if (query.trim().startsWith("mutation")) {
       result = await serverClient.mutate({
-        mutation: gql`${query}`,
+        mutation: gql`
+          ${query}
+        `,
         variables,
       });
     } else {
       result = await serverClient.query({
-        query: gql`${query}`,
+        query: gql`
+          ${query}
+        `,
         variables,
       });
     }
 
-    // console.log("result: ", result)
     const data = result.data;
-    // console.log("Query result: ", data);
 
     return NextResponse.json({ data }, { headers: corsHeaders });
   } catch (error) {
     console.error("GraphQL request error: ", error);
 
     // Return a meaningful error response
-    const errorMessage = error instanceof ApolloError ? error.message : "Internal server error.";
-    return NextResponse.json({ error: errorMessage }, { status: 500 });
+    const errorMessage =
+      error instanceof ApolloError ? error.message : "Internal server error.";
+    return NextResponse.json(
+      { error: errorMessage },
+      { status: 500, headers: corsHeaders }
+    ); // Add CORS headers to error response
   }
 }
